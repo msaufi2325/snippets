76057ee19d89b7020c597f4b834aa366438dda2d
Saufi <msaufi2325@gmail.com>
2023-12-21 21:34:40 +0900

    section 6

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -21,29 +21,42 @@ export async function deleteSnippet(id: number) {
 }
 
 export async function createSnippet(formState: { message: string }, formData: FormData) {
-  // Check the user's inputs and make sure they're valid
-  const title = formData.get('title')
-  const code = formData.get('code')
+  try {
+    // Check the user's inputs and make sure they're valid
+    const title = formData.get('title')
+    const code = formData.get('code')
 
-  if (typeof title !== 'string' || title.length < 3) {
-    return {
-      message: "Title must be longer"
+    if (typeof title !== 'string' || title.length < 3) {
+      return {
+        message: "Title must be longer"
+      }
+    }
+
+    if (typeof code !== 'string' || code.length < 10) {
+      return {
+        message: "Code must be longer"
+      }
+    }
+
+    // Create a new record in the database
+    await db.snippet.create({
+      data: {
+        title,
+        code
+      }
+    })
+
+  } catch (err: unknown) {
+    if (err instanceof Error) {
+      return {
+        message: err.message
+      }
+    } else {
+      return {
+        message: "Something went wrong..."
+      }
     }
   }
-
-  if (typeof code !== 'string' || code.length < 10) {
-    return {
-      message: "Code must be longer"
-    }
-  }
-
-  // Create a new record in the database
-  const snippet = await db.snippet.create({
-    data: {
-      title,
-      code
-    }
-  })
 
   // Redirect the user back to the root route
   redirect('/')
